975208eb20c92e593b680b7c81943fc60a3728f8
Kory Newton <korybnewton@gmail.com>
2019-09-13 15:20:39 -0700

    sign in form and custom button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component';
-
+import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
 import './App.css';
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -17,6 +17,9 @@ const Header = () => (
       <Link className="option" to="/shop">
         CONTACT
       </Link>
+      <Link className="option" to="/signin">
+        SIGN IN
+      </Link>
     </div>
   </div>
 );
