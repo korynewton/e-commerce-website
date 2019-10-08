71f0870b79dbd49afea86bfdb1759b722f813889
Kory Newton <korybnewton@gmail.com>
2019-10-07 17:18:03 -0700

    data normalization, using match params to display collections

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -6,3 +6,9 @@ export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections
 );
+
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+  );
